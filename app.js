c47c89a513f3b62a42a2b75daf3b1875a624948c
matrzask <maciejtrzask@student.agh.edu.pl>
2025-01-23 00:57:57 +0100

    socketio fixed

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
+
 app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 3000;
--- socketio.js
+++ socketio.js
@@ -1,12 +1,24 @@
 const socketIo = require('socket.io');
+const jwt = require('jsonwebtoken');
+const User = require('./models/userModel.js');
 
 module.exports = (server) => {
-    const io = socketIo(server);
+    const io = socketIo(server,
+        {
+            cors: {
+                origin: '*',
+                methods: ['GET', 'POST'],
+            }
+        }
+    );
 
     io.on('connection', async (socket) => {
+        console.log('New connection');
         const token = socket.handshake.query.token; // token passed in query
+
         if (!token) {
             socket.disconnect(true);
+            console.log('No token provided');
             return
         }
 
@@ -15,6 +27,7 @@ module.exports = (server) => {
             const user = await User.findById(decoded.id).select('-password');
 
             if (!user) {
+                console.log('Invalid token');
                 socket.disconnect(true);
                 return;
             }
@@ -27,7 +40,11 @@ module.exports = (server) => {
             socket.on('disconnect', () => {
                 console.log(`User ${user._id} disconnected`);
             });
+            socket.on('message', (data) => {
+                console.log(`Received message from user ${user._id}:`, data);
+            });
         } catch (error) {
+            console.log('Error during token verification or user retrieval:', error);
             socket.disconnect(true);
         }
     });
