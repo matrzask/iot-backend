595790161f2b5e78d3ba71bc7943497af1ec48ab
matrzask <maciejtrzask@student.agh.edu.pl>
2025-01-22 12:42:27 +0100

    gps mqtt with conversion

--- mqttClient.js
+++ mqttClient.js
@@ -1,6 +1,14 @@
 var mqtt = require('mqtt');
 var client  = mqtt.connect('mqtt:localhost:1883');
 
+function bytesToFloat(bytes) {
+    if (bytes.length !== 4) {
+        throw new Error('Invalid byte length for float conversion');
+      }
+    var buffer = Buffer.from(bytes);
+    return buffer.readFloatLE(0);
+  }
+
 client.on('connect', function () {
   console.log('Connected to MQTT broker');
   client.subscribe('/info/connect', function (err) {
@@ -17,5 +25,15 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(`Received message: ${message.toString()} on topic: ${topic}`);
+  if(topic.startsWith('/gps/')) {
+    try {
+      let value = bytesToFloat(message);
+      console.log(`Received message: ${value} on topic: ${topic}`);
+    } catch (error) {
+      console.error(`Error processing message on topic ${topic}: ${error.message}`);
+    }
+  }
+  else {
+    console.log(`Received message: ${message.toString()} on topic: ${topic}`);
+  }
 });
